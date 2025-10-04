a040a614ec4f6ae1d0602d461d590751842ad6d9
oni <oni@yumi.fu>
2025-10-04 22:11:14 +0330

    2nd script fix

--- z_change_name.sh
+++ z_change_name.sh
@@ -3,20 +3,20 @@
 # having changed their appname and db creds for the new environment
 
 # reading creds
-# echo "Application Name:"
-# x_appname=$(python3 -c "print(input())")
+echo "Application Name:"
+x_appname=$(python3 -c "print(input())")
 
-# echo "Database Name:"
-# x_dbname=$(python3 -c "print(input())")
+echo "Database Name:"
+x_dbname=$(python3 -c "print(input())")
 
-# echo "Username for DB:"
-# x_username=$(python3 -c "print(input())")
+echo "Username for DB:"
+x_username=$(python3 -c "print(input())")
 
-# echo "Password for DB:"
-# x_password=$(python3 -c "print(input())")
+echo "Password for DB:" 
+x_password=$(python3 -c "print(input())")
 
 # inline editing everything
-grep -Hnre "XX_APP" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq
+# grep -Hnre "XX_APP" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq
 grep --color=NEVER -Hr "XX_APP_NAME" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq | xargs sed -i "s/XX_APP_NAME/$x_appname/g"
 grep --color=NEVER -Hr "XX_APP_USERNAME" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq | xargs sed -i "s/XX_APP_USERNAME/$x_username/g"
 grep --color=NEVER -Hr "XX_APP_PASSWORD" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq | xargs sed -i "s/XX_APP_PASSWORD/$x_password/g"
