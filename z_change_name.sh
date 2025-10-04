911b6d30b9a3593894d1b12f67b5a494aada4359
oni <oni@yumi.fu>
2025-10-04 22:09:28 +0330

    fixed the script

--- z_change_name.sh
+++ z_change_name.sh
@@ -23,8 +23,11 @@ grep --color=NEVER -Hr "XX_APP_PASSWORD" | awk '{print $1}' | awk 'BEGIN{FS=":";
 grep --color=NEVER -Hr "XX_APP_DBNAME" | awk '{print $1}' | awk 'BEGIN{FS=":";} {print "./" $1}' | uniq | xargs sed -i "s/XX_APP_DBNAME/$x_dbname/g"
 
 # changing directory names
-# dirs=$(find . -name \*XX_APP_NAME\* | xargs realpath )
-# for d in $dirs; do
-	# destname=$(echo $d| sed "s/XX_APP_NAME/$x_appname/")
-	# mv $d $destname
-# done
+dirs=$(find . -name \*XX_APP_NAME\* | xargs realpath )
+for d in $dirs; do
+	destname=$(echo $d| sed "s/XX_APP_NAME/$x_appname/")
+	mv $d $destname
+done
+
+rm -rf .git
+rm .gitignore
